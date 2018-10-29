b084094d2f50c6b29227045cd05dcda962d56970
slackscassidy <44562593+slackscassidy@users.noreply.github.com>
2018-10-29 13:31:27 +0800

    PINKBOOKBOT

---
+++ PINKBOOKS/config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'H5bmAij92jyWdMhW821upQgoI',
+  consumer_secret:      'DN8PwvZittY4dQpTem7zIwqKTqfDjSUbTqUS4KzeEa30HbUJTu',
+  access_token:         '969293015989800961-yC0eDPiAyKsShpMpNXk3IpTT7PeRD4Z',
+  access_token_secret:  'bWTAt4ZTzGMWtJzJvfMdLa5LuabvHB5zWOmOc90SXxlcW'
+}
+
+module.exports = config;
