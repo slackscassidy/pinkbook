5f08227f7ed5c1d8d1a5f29b37676f9ad828120c
slackscassidy <44562593+slackscassidy@users.noreply.github.com>
2018-10-31 14:07:28 +0800

    Update server.js

--- PINKBOOKS/server.js
+++ PINKBOOKS/server.js
@@ -3,7 +3,12 @@ var Twit = require('twit')
 var fs = require('fs'),
     path = require('path'),
     Twit = require('twit'),
-    config = require(path.join(__dirname, 'config.js'));
+    config = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+};
 
 var T = new Twit(config);
 
